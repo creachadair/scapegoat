41ee0529a15fc88af44484babb651a7faf81ea10
M. J. Fromberger <michael.j.fromberger@gmail.com>
2016-08-30 01:04:10 -0700

    Add ordered benchmarks.

--- benchmark_test.go
+++ benchmark_test.go
@@ -16,23 +16,15 @@ func benchTree(β int) (*Tree, *rand.Rand) {
 	return New(β), rand.New(rand.NewSource(benchSeed))
 }
 
-func benchInsert(b *testing.B, β int) *Tree {
-	tree, rng := benchTree(β)
-	for i := 0; i < b.N; i++ {
-		tree.Insert(Z(rng.Intn(math.MaxInt32)))
+func orderedTree(b *testing.B, β int) *Tree {
+	tree := New(β)
+	for i := 1; i <= b.N; i++ {
+		tree.Insert(Z(i))
 	}
 	return tree
 }
 
-func BenchmarkInsert(b *testing.B) {
-	b.Run("balance", func(b *testing.B) {
-		for _, β := range balances {
-			b.Run(fmt.Sprint(β), func(b *testing.B) { benchInsert(b, β) })
-		}
-	})
-}
-
-func fullTree(b *testing.B, β int) (*Tree, []int) {
+func randomTree(b *testing.B, β int) (*Tree, []int) {
 	tree, rng := benchTree(β)
 	values := make([]int, b.N)
 	for i := 0; i < b.N; i++ {
@@ -42,34 +34,79 @@ func fullTree(b *testing.B, β int) (*Tree, []int) {
 	return tree, values
 }
 
-func benchRemove(b *testing.B, β int) {
-	tree, values := fullTree(b, β)
-	b.ResetTimer()
-	for _, v := range values {
-		tree.Remove(Z(v))
-	}
-}
-
-func BenchmarkRemove(b *testing.B) {
+func BenchmarkInsertRandom(b *testing.B) {
 	b.Run("balance", func(b *testing.B) {
 		for _, β := range balances {
-			b.Run(fmt.Sprint(β), func(b *testing.B) { benchRemove(b, β) })
+			b.Run(fmt.Sprint(β), func(b *testing.B) {
+				tree, rng := benchTree(β)
+				for i := 0; i < b.N; i++ {
+					tree.Insert(Z(rng.Intn(math.MaxInt32)))
+				}
+			})
 		}
 	})
 }
 
-func benchLookup(b *testing.B, β int) {
-	tree, values := fullTree(b, β)
-	b.ResetTimer()
-	for _, v := range values {
-		tree.Lookup(Z(v))
-	}
-}
-
-func BenchmarkLookup(b *testing.B) {
+func BenchmarkInsertOrdered(b *testing.B) {
 	b.Run("balance", func(b *testing.B) {
 		for _, β := range balances {
-			b.Run(fmt.Sprint(β), func(b *testing.B) { benchLookup(b, β) })
+			b.Run(fmt.Sprint(β), func(b *testing.B) { orderedTree(b, β) })
+		}
+	})
+}
+
+func BenchmarkRemoveRandom(b *testing.B) {
+	b.Run("balance", func(b *testing.B) {
+		for _, β := range balances {
+			b.Run(fmt.Sprint(β), func(b *testing.B) {
+				tree, values := randomTree(b, β)
+				b.ResetTimer()
+				for _, v := range values {
+					tree.Remove(Z(v))
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkRemoveOrdered(b *testing.B) {
+	b.Run("balance", func(b *testing.B) {
+		for _, β := range balances {
+			b.Run(fmt.Sprint(β), func(b *testing.B) {
+				tree := orderedTree(b, β)
+				b.ResetTimer()
+				for v := 1; v <= b.N; v++ {
+					tree.Remove(Z(v))
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkLookupRandom(b *testing.B) {
+	b.Run("balance", func(b *testing.B) {
+		for _, β := range balances {
+			b.Run(fmt.Sprint(β), func(b *testing.B) {
+				tree, values := randomTree(b, β)
+				b.ResetTimer()
+				for _, v := range values {
+					tree.Lookup(Z(v))
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkLookupOrdered(b *testing.B) {
+	b.Run("balance", func(b *testing.B) {
+		for _, β := range balances {
+			b.Run(fmt.Sprint(β), func(b *testing.B) {
+				tree := orderedTree(b, β)
+				b.ResetTimer()
+				for v := 1; v <= b.N; v++ {
+					tree.Lookup(Z(v))
+				}
+			})
 		}
 	})
 }
