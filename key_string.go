01d995ac1986cb6b88416ac8f707a96315fcca47
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-11 20:59:36 -0700

    better document the default key type

--- key_string.go
+++ key_string.go
@@ -1,6 +1,8 @@
 package scapegoat
 
-// Key defines a string key for a scapegoat tree.
+// Key defines a string key for a scapegoat tree. This is the default key type
+// for the base package in the module. Use the mktree tool to generate packages
+// for other key types.
 type Key = string
 
 // keyLess reports whether a is ordered prior to b.
