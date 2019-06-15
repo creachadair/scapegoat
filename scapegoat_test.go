d8137c9362df631385dc986ecffb592c138d2326
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-15 12:49:57 -0700

    Use a larger test sample.

--- scapegoat_test.go
+++ scapegoat_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -111,29 +112,11 @@ func TestNewKeys(t *testing.T) {
 
 func TestBasicProperties(t *testing.T) {
 	// http://www.gutenberg.org/files/1063/1063-h/1063-h.htm
-	tree, words := makeTree(*strictness, `
-The thousand injuries of Fortunato I had borne as I best could but when he
-ventured upon insult I vowed revenge You who so well know the nature of my soul
-will not suppose however that gave utterance to a threat At length I would be
-avenged this was a point definitely settled but the very definitiveness with
-which it was resolved precluded the idea of risk I must not only punish but
-punish with impunity A wrong is unredressed when retribution overtakes its
-redresser It is equally unredressed when the avenger fails to make himself felt
-as such to him who has done the wrong
-
-It must be understood that neither by word nor deed had I given Fortunato cause
-to doubt my good will I continued as was my in to smile in his face and he did
-not perceive that my to smile now was at the thought of his immolation.
-
-He had a weak point this Fortunato although in other regards he was a man to be
-respected and even feared He prided himself on his connoisseurship in wine Few
-Italians have the true virtuoso spirit For the most part their enthusiasm is
-adopted to suit the time and opportunity to practise imposture upon the British
-and Austrian millionaires In painting and gemmary Fortunato like his countrymen
-was a quack but in the matter of old wines he was sincere In this respect I did
-not differ from him materially I was skilful in the Italian vintages myself and
-bought largely whenever I could`)
-
+	text, err := ioutil.ReadFile("cask.txt")
+	if err != nil {
+		t.Fatalf("Reading text: %v", err)
+	}
+	tree, words := makeTree(*strictness, string(text))
 	t.Logf("Final tree has size %d; height %d", tree.Len(), tree.root.height())
 	dumpTree(tree)
 
